a305f6588667d623dfaaec98e8155c9cff3449cb
STEPAN <chichetka27@gmail.com>
2022-06-11 19:23:34 +0400

    add Problem №16 CheckSquared

---
+++ P_15_DayWeek/Program.cs
@@ -0,0 +1,21 @@
+﻿/*  Написать программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот 
+день выходным: 6 -> да; 7 -> да; 1 -> нет */
+
+
+Console.Clear();
+Console.Write("Введите число от 1 до 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 1 || num > 7)
+{
+    Console.Write("ошибка ввода");
+}
+else if (num == 6 || num == 7)
+    {
+        Console.Write("да");
+    }
+    else 
+    {
+        Console.Write("нет");
+    }
---
+++ P_16_CheckSquared/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает на вход 
+два числа и проверяет, является ли одно число 
+квадратом другого: 5, 25 -> да; -4, 16 -> да; 
+25, 5 -> да; 8,9 -> нет  */
+
+Console.Clear();
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int numSquared1 = num1 * num1;
+int numSquared2 = num2 * num2;
+
+if (num1 == numSquared2 || num2 == numSquared1)
+{
+    Console.Write("ДА");
+}
+else
+{
+    Console.Write("НЕТ");
+}
