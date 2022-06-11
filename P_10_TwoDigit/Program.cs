b0ebd4cbe9557eefc3589a6162c38eb4ab131194
STEPAN <chichetka27@gmail.com>
2022-06-11 15:22:11 +0400

    add correct Problem №10 TwoDigit

--- P_10_TwoDigit/Program.cs
+++ P_10_TwoDigit/Program.cs
@@ -7,6 +7,11 @@ Console.Clear();
 Console.Write("Введите трёхзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine ());
 
+if (num < -99)
+{
+    num = num * -1;
+}
+
 if (num > 99 && num < 1000)
 {
     num = num / 10 - num / 100 * 10;
