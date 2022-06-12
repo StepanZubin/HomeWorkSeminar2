5cba2416217dcac28314c3b3c5ed51c019a4804c
STEPAN <chichetka27@gmail.com>
2022-06-12 06:43:22 +0400

    add methods Problem №16 CheckSquared

--- P_16_CheckSquared/Program.cs
+++ P_16_CheckSquared/Program.cs
@@ -3,6 +3,7 @@
 квадратом другого: 5, 25 -> да; -4, 16 -> да; 
 25, 5 -> да; 8,9 -> нет  */
 
+/*
 Console.Clear();
 Console.Write("Введите число 1: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -19,4 +20,31 @@ if (num1 == numSquared2 || num2 == numSquared1)
 else
 {
     Console.Write("НЕТ");
+}
+*/
+
+
+Console.Clear();
+int num1 = Input("Введите число 1: ");
+int num2 = Input("Введите число 2: ");
+
+if (num1 == Squared(num2) || num2 == Squared(num1))
+{
+    Console.Write("ДА");
+}
+else
+{
+    Console.Write("НЕТ");
+}
+
+
+int Input(string lineOutput)  // Метод ввода
+{
+    Console.Write(lineOutput);
+    return (Convert.ToInt32(Console.ReadLine()));
+}
+
+int Squared(int n)  // Метод возведения в квадрат
+{
+    return (n * n);
 }
